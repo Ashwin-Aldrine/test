ce3b6f145467c797829e33f992cc8d592faf00d5
Ashwin-Aldrine <ashwinlenaldrine@gmail.com>
2024-11-20 05:58:58 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,8 @@
+from picamera import Picamera
+from time import sleep
+camera=Picamera()
+camera.start_preview()
+for i in range(5):
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image%s.jpg'%i)
+camera.stop_preview()
